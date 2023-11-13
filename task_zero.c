512c49e711f164a28e2161e6764c689b6c8aff25
Martttexxx <martinnwosu601@gmail.com>
2023-11-13 05:27:27 -0800

    task_zero.c

---
+++ task_zero.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+*mul - multiplies two intergers
+*@num1: first interger to be multiplied
+*@num2: second interger to be multiplied
+*Return: result of the multiplication
+*/
+
+int mul(int num1, int num2)
+{
+	int num1;
+	int num2;
+
+	return (num1 * num2);
+}
+
+
